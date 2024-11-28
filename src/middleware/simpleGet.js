d7a00de65ce069c8a5c28ad566de65efbbaeaf40
colourful <colourful4570@163.com>
2024-11-28 15:26:18 +0800

    get请求对params为对象格式的参数处理

--- src/middleware/simpleGet.js
+++ src/middleware/simpleGet.js
@@ -1,3 +1,55 @@
-export default function simpleGetMiddleware(){
+import {isURLSearchParams, isArray, forEach2ObjArr, isDate, isObject, reqStringify} from '../utils';
 
+export function paramsSerialize(params, paramsSerializer){
+    let serializedParams;
+    let jsonStringifiedParams;
+    if(params){
+        if(paramsSerializer){
+
+        }else if(isURLSearchParams(params)){
+
+        }else{
+            if(isArray(params)){
+
+            }else{
+                jsonStringifiedParams = {};
+                forEach2ObjArr(params, function(value,key){
+                    let jsonStringifiedValue = value;
+                    if(value === null || typeof value === 'undefined'){
+                        jsonStringifiedParams[key] = value;
+                    } else if(isDate(value)){
+                        jsonStringifiedValue = value.toISOString();
+                    }else if (isArray(value)){
+                        jsonStringifiedValue = value;
+                    }else if(isObject(value)){
+                        jsonStringifiedValue = JSON.stringify(value);
+                    }
+                    jsonStringifiedParams[key] = jsonStringifiedValue;
+                })
+                serializedParams = reqStringify(jsonStringifiedParams);
+            }
+        }
+    }
+
+    return serializedParams;
+}
+
+// 中间件处理GET请求
+export default function simpleGetMiddleware(ctx,next){
+    if(!ctx) return next();
+    const { req: { options = {}} = {} } = ctx;
+    const {  paramsSerializer, params } = options;
+    let {req: {url = ''} = {}} = ctx;
+
+    options.method = options.method ? options.method.toUpperCase() : 'GET';
+    options.credentials = options.credentials || 'same-origin';
+
+    let serializedParams = paramsSerialize(params, paramsSerializer);
+    ctx.req.originUrl = url;
+    if(serializedParams){
+        const urlSign = url.indexOf('?') === -1 ? '?' : '&';
+        ctx.req.url = `${url}${urlSign}${serializedParams}`;
+    }
+    ctx.req.options = options;
+    return next();
 }
--- src/utils.js
+++ src/utils.js
@@ -89,3 +89,29 @@ export function getEnv(){
 export function cancel2Throw(opt){
 
 }
+export function isArray(val){
+    return typeof val === 'object' && Object.prototype.toString.call(val) === '[object Array]';
+}
+export function isObject(val){
+    return val !== null && typeof val === 'object';
+}
+export function isDate(val){
+    return typeof val === 'object' && Object.prototype.toString.call(val) === '[object Date]';
+}
+export function forEach2ObjArr(target, callback){
+    if(!target) return;
+    if(typeof target !== 'object'){
+        target = [target];
+    }
+    if(isArray(target)){
+        for (let i = 0; i < target.length; i++) {
+            callback.call(null, target[i], i, target);
+        }
+    }else{
+        for (let key in target){
+            if(Object.prototype.hasOwnProperty.call(target, key)){
+                callback.call(null, target[key], key, target);
+            }
+        }
+    }
+}
